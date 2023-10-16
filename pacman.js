79745ec4048061102dadf8d79c86e5d02cc3691f
MutharasiMsccs <140373908+MutharasiMsccs@users.noreply.github.com>
2023-10-16 10:35:54 +0530

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,7 +1,7 @@
 let pageWidth = window.innerWidth;
 const pacArray = [
-  ["./images/PacMan1.png", "./images/PacMan2.png"],
-  ["./images/PacMan3.png", "./images/PacMan4.png"],
+  ["PacMan1.png", "PacMan2.png"],
+  ["PacMan3.png", "PacMan4.png"],
     ];
     var direction = 0;
     var focus = 0;
